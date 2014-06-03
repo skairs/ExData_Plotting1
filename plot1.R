4bd7bf581b0c0430a8ce7c77a10454508ab68964
skairs <stefanie.ness@gmail.com>
2014-06-02 17:42:48 -0700

    data import rough draft

---
+++ plot1.R
@@ -0,0 +1,22 @@
+## 02 June, 2014 -- SNK
+## Coursera, Exploratory Data Analysis 
+## Programming Assignment 1 - Plot 1
+
+## data file name:
+filename <- household_power_consumption.txt
+
+## read in data file. Note: separator is semi-colon
+data <- read.csv(filename, header = TRUE, sep = ";", na.strings="?")
+
+## convert dates to %Y-%m-%d format from %d/%m/%Y format
+data[,1] <- as.Date(data[,1], format="%d/%m/%Y")
+
+## create a data filter:
+filter <- c("2007-02-01", "2007-02-02")
+
+## subset by requested date range:
+feb_data <- data[((data[,1]==filter[1]) | (data[,1]==filter[2])),]
+
+## close but not quite
+##hist(data[,3], col = "red", xlab="Global Active Power (kilowatts)")
+
